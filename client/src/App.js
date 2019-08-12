15411db3fb03d401c856cc49f6c5b8eb1ac5f648
Piotr Nowinski <pitred007git@gmail.com>
2019-08-12 21:45:48 +0200

    Add next route

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MainLayout from './components/layouts/MainLayout/MainLayout';
+import Home from './components/pages/Home/HomePage';
+import Posts from './components/pages/Posts/PostsPage';
+import SinglePost from './components/pages/SinglePost/SinglePostPage';
+import AddPost from './components/pages/AddPost/AddPostPage';
+import Contact from './components/pages/Contact/ContactPage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
 
 class App extends React.Component {
-   state = {
-      posts: []
-   };
-
-   componentDidMount() {
-      fetch('http://localhost:8000/api/posts')
-         .then(res => res.json())
-         .then(res => {
-            this.setState({ posts: res });
-         });
-   }
-
    render() {
       return (
-         <div>
-            <ul>
-               {this.state.posts.map(post => (
-                  <li key={post.id}>{post.title}</li>
-               ))}
-            </ul>
-         </div>
+         <MainLayout>
+            <Switch>
+               <Route path='/' exact component={Home} />
+               <Route path='/posts' exact component={Posts} />
+               <Route path='/posts/single_post' exact component={SinglePost} />
+               <Route path='/posts/new' exact component={AddPost} />
+               <Route path='/contact' exact component={Contact} />
+               <Route component={NotFound} />
+            </Switch>
+         </MainLayout>
       );
    }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.scss';
+import 'animate.css/animate.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const Root = () => <App />;
+const Root = () => (
+   <Router>
+      <App />
+   </Router>
+);
 ReactDOM.render(<Root />, document.getElementById('root'));
