fb36b583ae207a1b40167004036de944256316be
Piotr Nowinski <pitred007git@gmail.com>
2019-08-24 16:06:23 +0200

    Add more logic 31.4

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const mongoSanitize = require('mongo-sanitize');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const loadTestData = require('./testData');
 
