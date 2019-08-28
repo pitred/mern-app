fe2635383012d7b3bc6cc67d94af8466ba5d8f8f
Piotr Nowinski <pitred007git@gmail.com>
2019-08-28 10:39:11 +0200

    Add Heroku

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-   PORT: 8000,
-   DB: 'mongodb+srv://RedDB-user:redone@clusterred-auixb.mongodb.net/test?retryWrites=true&w=majority'
+   PORT: process.env.PORT,
+   DB: process.env.DB
 };
