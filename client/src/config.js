19d5d0495fbddbfe62b8bae7069b67d94aa8e5b6
Piotr Nowinski <pitred007git@gmail.com>
2019-08-28 21:41:54 +0200

    Add Heroku

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
-export const BASE_URL = 'http://localhost:3000/';
+export const API_URL = '/api';
+export const BASE_URL = 'https://mern-red-app.herokuapp.com';
